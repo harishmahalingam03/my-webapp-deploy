9a368d5bf78a180d170362ab63c8eab16b0b8a99
harishmahalingam03 <harish.m@atomedgesoft.com>
2024-09-17 16:24:40 +0530

    Delete scripts/deploy_container.sh

--- scripts/deploy_container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-# Pull the latest image from ECR and run it
-aws ecr get-login-password --region $AWS_DEFAULT_REGION | sudo docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com
-docker pull $ECR_REGISTRY:latest
-docker stop static-website || true
-docker rm static-website || true
-docker run -d --name static-website -p 80:80 $ECR_REGISTRY:latest
